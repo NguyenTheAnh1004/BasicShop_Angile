bf9ab4ac717ba801e100cdf2e911c270bc850ce2
anderson113 <73542040+anderson113@users.noreply.github.com>
2022-03-11 21:39:06 +0700

    push pj

---
+++ MVC/Core/Config.php
@@ -0,0 +1,19 @@
+   <?php  
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_HOST', 'localhost');
+	define('DB_SERVERNAME', 'dotshop');
+
+// User define
+	define('BASE_URL','http://localhost/BasicShop_Angile/');
+	define('CSS_URL','http://localhost/BasicShop_Angile/Public/css');
+	define('IMAGE_URL','http://localhost/BasicShop_Angile/Public/images');
+	define('JS_URL','http://localhost/BasicShop_Angile/Public/js');
+
+// Admin define
+	define('ADMIN_BASE_URL','http://localhost/BasicShop_Angile/Admin/');
+	define('ADMIN_CSS_URL','http://localhost/BasicShop_Angile/Public/admin/css');
+	define('ADMIN_IMAGE_URL','http://localhost/BasicShop_Angile/Public/admin/images');
+	define('ADMIN_JS_URL','http://localhost/BasicShop_Angile/Public/admin/js');
+
+?>
